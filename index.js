fb4734dabe894264dccba3a67747b7a827127e92
thiago <teagapk@gmail.com>
2021-09-20 13:07:42 -0300

    Aprendendo sobre objetos no javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+//           Chave.  Valor.
+var rafa = { nome: 'Rafa', vitorias: 2, empates: 1, derrotas: 1, pontos: 0 }
+var thiago = {
+  nome: 'thiago',
+  vitorias: 4,
+  empates: 0,
+  derrotas: 01,
+  pontos: 100
+} // O que está dentro de chaves são objetvos
+console.log(rafa)
+console.log(thiago)
