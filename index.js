c99def4032fe070dae4cee6f4d28bd2b67dbf3be
thiago <teagapk@gmail.com>
2021-09-20 14:54:36 -0300

    Aprendendo a cria a estrutura lógica que a tabela terá

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ var rafa = { nome: 'Rafa', vitorias: 2, empates: 1, derrotas: 1, pontos: 0 }
 var thiago = {
   nome: 'thiago',
   vitorias: 4,
-  empates: 0,
-  derrotas: 01,
-  pontos: 100
-} // O que está dentro de chaves são objetvos
-console.log(rafa)
+  empates: 8,
+  derrotas: 06,
+  pontos: 3
+}
+// O que está dentro de chaves são objetvos
+// console.log(rafa)
+// console.log(thiago)
+
 console.log(thiago)
+var resultadoDosPontos
+calculaPontos(thiago)
+
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates
+  return pontos
+}
